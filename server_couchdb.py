33da6c0c093b80ce6b3484c92b8bd1595e83c320
Jack Muratore <jackmuratore@gmail.com>
2016-10-19 00:17:08 -0400

    Clean up couchdb - DRY

--- server_couchdb.py
+++ server_couchdb.py
@@ -8,24 +8,9 @@ from tornado import gen
 from tornado.concurrent import run_on_executor
 
 
-class CouchHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
     couch = couchdb.Server('http://couchdb:5984')
 
-    def post(self):
-        msg = self.request.body.decode('utf-8')
-        data = json.loads(msg)
-        if 'test' not in self.couch:
-            self.couch.create('test')
-        db = self.couch['test']
-        db.save(data)
-        self.write('{"result": "success"}')
-
-
-class CouchAsyncHandler(tornado.web.RequestHandler):
-    couch = couchdb.Server('http://couchdb:5984')
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=8)
-
-    @run_on_executor
     def write_couch(self):
         msg = self.request.body.decode('utf-8')
         data = json.loads(msg)
@@ -34,7 +19,22 @@ class CouchAsyncHandler(tornado.web.RequestHandler):
         db = self.couch['test']
         db.save(data)
 
+
+class CouchHandler(BaseHandler):
+
+    def post(self):
+        self.write_couch()
+        self.write('{"result": "success"}')
+
+
+class CouchAsyncHandler(BaseHandler):
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=8)
+
+    @run_on_executor
+    def async_write_couch(self):
+        self.write_couch()
+
     @gen.coroutine
     def post(self):
-        yield self.write_couch()
+        yield self.async_write_couch()
         self.write('{"result": "success"}')
