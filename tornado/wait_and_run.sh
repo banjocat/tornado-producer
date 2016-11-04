21765621edfe43af3e13e967c19f934c54364eb8
Jack Muratore <jackmuratore@gmail.com>
2016-11-03 23:58:30 -0400

    Add rabbitmq and proper exit on failure flag

--- tornado/wait_and_run.sh
+++ tornado/wait_and_run.sh
@@ -1,5 +1,7 @@
+#!/bin/sh -e
 ./wait-for-it.sh kafka:9092
 ./wait-for-it.sh mongo:27017
 ./wait-for-it.sh elastic:9200
+./wait-for-it.sh rabbit:5672
 ./wait-for-it.sh cassandra:9042 -t 30
 pypy3 server.py
