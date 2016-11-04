e00717627874be91f4133d88e7620e187b737a5f
Jack Muratore <jackmuratore@gmail.com>
2016-11-03 23:59:31 -0400

    Correct name of rabbitmq service

--- tornado/wait_and_run.sh
+++ tornado/wait_and_run.sh
@@ -2,6 +2,6 @@
 ./wait-for-it.sh kafka:9092
 ./wait-for-it.sh mongo:27017
 ./wait-for-it.sh elastic:9200
-./wait-for-it.sh rabbit:5672
+./wait-for-it.sh rabbitmq:5672
 ./wait-for-it.sh cassandra:9042 -t 30
 pypy3 server.py
